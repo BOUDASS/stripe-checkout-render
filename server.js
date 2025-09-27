a98d21aff261fcf9fce9d721bba89814ebbbf1ce
boudass <boudassothmane@gmail.com>
2025-09-28 00:52:51 +0200

    update

--- server.js
+++ server.js
@@ -7,45 +7,40 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/create-checkout-session', async (req, res) => {
-  const { panier } = req.body;
- console.log("retrieving sessionId", panier,process.env.STRIPE_SECRET_KEY);
+    const { panier } = req.body;
+    console.log("retrieving sessionId", panier, process.env.STRIPE_SECRET_KEY);
 
-  try {
-    const session = await stripe.checkout.sessions.create({
-      /* payment_method_types: ['card'],
-      line_items: panier.map(item => ({
-        price: item.id,
-        quantity: item.quantity
-      })),
-      mode: 'payment',
-      success_url: 'https://votresite.com/success',
-      cancel_url: 'https://votresite.com/cancel', */
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: panier.map(item => ({
+                price_data: {
+                    currency: 'eur',
+                    product_data: {
+                        name: item.id,
+                    },
+                    unit_amount: 2000,
+                },
+                quantity: item.quantity
+            })),
+            mode: 'payment',
+            success_url: `${req.headers.origin}/success`,
+            cancel_url: `${req.headers.origin}/cancel`,
 
-      payment_method_types: ['card'],
-  mode: 'payment',
-  line_items: [{
-    price_data: {
-      currency: 'eur',
-      product_data: {
-        name: 'Produit Exemple',
-      },
-      unit_amount: 2000,
-    },
-    quantity: 1,
-  }],
-  success_url: `${req.headers.origin}/success`,
-  cancel_url: `${req.headers.origin}/cancel`,
-    });
-    //console.error(error);
-    res.json({ id: session.id });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erreur de session' });
-  }
+
+
+
+        });
+        //console.error(error);
+        res.json({ id: session.id });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erreur de session' });
+    }
 });
 
 app.get('/', (req, res) => {
-  res.send('API Stripe en ligne 🚀');
+    res.send('API Stripe en ligne 🚀');
 });
 
 const PORT = process.env.PORT || 3000;
